f8fd9e21a9d4fdf8a5f2ac96dd4c317f71ce4dc5
intersoom <sallybig@naver.com>
2024-06-21 04:53:44 +0900

    feat: add home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+
+import { AppConfig } from '@/utils/AppConfig';
+
 import { HOME_INFOS } from '../const/home';
 
 export async function generateMetadata() {
@@ -8,7 +12,47 @@ export async function generateMetadata() {
 
 const Home = () => (
   <div className="py-12">
-    <div className="mx-auto h-screen overflow-auto px-12">
+    <div className="py-8">
+      <h1 className="text-3xl font-normal text-white">{AppConfig.name}</h1>
+      <h2 className="text-lg text-gray-400">삶을 끝맺는 공간을 지나 죽음을 끝맺다</h2>
+    </div>
+    <div className="mx-auto h-fit px-12">
+      <Image
+        width={5906}
+        height={5906}
+        src="/assets/images/1.webP"
+        alt="Picture of me"
+        className="w-80"
+        // placeholder="blur"
+        // blurDataURL="/assets/images/1.webP" // 추후 수정
+      />{' '}
+      <Image
+        width={3508}
+        height={4961}
+        src="/assets/images/2.webP"
+        alt="Picture of me"
+        className="w-80"
+        // placeholder="blur"
+        // blurDataURL="/assets/images/2.webP" // 추후 수정
+      />{' '}
+      <Image
+        width={2268}
+        height={1135}
+        src="/assets/images/3.webP"
+        alt="Picture of me"
+        className="w-80"
+        // placeholder="blur"
+        // blurDataURL="/assets/images/3.webP" // 추후 수정
+      />
+      <Image
+        width={472}
+        height={1135}
+        src="/assets/images/4.webP"
+        alt="Picture of me"
+        className="w-80"
+        // placeholder="blur"
+        // blurDataURL="/assets/images/4.webP" // 추후 수정
+      />
       <p>삶을 끝맺는 공간을 지나 죽음을 끝맺다.</p>
       <p>무연고자 사후처리시스템을 통해 새로운 형태의 도심 속 장사 시설을 제안한다.</p>
       {HOME_INFOS.map((info, _) => (
--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -1,31 +1,22 @@
-import { AppConfig } from '@/utils/AppConfig';
-
 const BaseTemplate = (props: {
   leftNav: React.ReactNode;
   rightNav?: React.ReactNode;
   children: React.ReactNode;
 }) => {
   return (
-    <div className="size-full bg-black px-1 text-white antialiased">
-      <div className="mx-auto w-full">
-        <header className="border-b border-gray-300 px-8 py-4">
-          <div className="pb-8 pt-16">
-            <h1 className="text-7xl font-normal text-white">{AppConfig.name}</h1>
-            <h2 className="text-lg text-gray-400">삶을 끝맺는 공간을 지나 죽음을 끝맺다</h2>
-          </div>
-          <div className="flex justify-between">
+    <div className="h-screen w-screen bg-black text-white antialiased">
+      <div className="mx-auto flex size-full flex-col">
+        <header className="min-h-fit border-b border-gray-300 p-8">
+          <div className="flex items-center justify-between">
             <nav>
-              <ul className="flex flex-wrap gap-x-5 text-4xl">{props.leftNav}</ul>
+              <ul className="flex flex-wrap gap-x-5 text-2xl">{props.leftNav}</ul>
             </nav>
-
             <nav>
-              <ul className="flex flex-wrap gap-x-5 text-4xl">{props.rightNav}</ul>
+              <ul className="flex flex-wrap gap-x-5 text-2xl">{props.rightNav}</ul>
             </nav>
           </div>
         </header>
-
-        <main>{props.children}</main>
-
+        <main className="h-full">{props.children}</main>
         <footer className="border-t border-gray-300 py-8 text-center text-sm">
           made by : 허제연 site by : 이수민
         </footer>
